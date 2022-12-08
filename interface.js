a3daf469ae89156603c0e597d2e223cc03d0b304
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-12-07 22:32:54 -0500

    core functinality completed

--- interface.js
+++ interface.js
@@ -1,88 +1,19 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/newEmployee');
 
-const addEmployee = () => {
-    console.log('==========Add A New Employee==========');
+const updateRole = () => {
+    console.log('========Update Employee Role==========');
     return inquirer.prompt([
         {
-            type: 'Input',
-            message: "What is the employee's first name?",
-            name: 'firstName',
+            type: "input",
+            message: "What is the employee's name?",
+            name: "employeeName",
         },
-        {
-            type: 'Input',
-            message: "What is the employee's last name?",
-            name: 'lastName',
-        },
-        {
-            type: 'Input',
-            message: "What is the employee's role?",
-            name: 'role',
-        },
-        {
-            type: 'Input',
-            message: "What is the employee's manager?",
-            name: 'manager',
-        },
-    ])
-    .then((response) => {
-         
-        // console.log(response);
-        const newHire = new Employee(response.firstName, response.lastName, response.role, response.manager)
-        newHire.printInfo();
-    });
-}
-
-const addRole = () => {
-    console.log('==============Add a New Role===========');
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: "What is the new role's title?",
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: "What is the new Role's salary?",
-            name: 'salary',
-        },
-        {
-            type: 'input',
-            message: "Which department does the new role belong to?",
-            name: 'department',
-        }
-    ])
-    .then((response) => {
-        const newRole = new Role(response.title, response.salary, response.department);
-        db.query('SELECT * FROM department WHERE name = ?', newRole, (err, result) => {
+    ]) .then((answer) =>{
+        let empID = answer.employeeName.split(" ");
+        db.query(`SELECT id FROM employee WWHERE first_name LIKE ? AND last_name LIKE?`, [empID[0], empID[1]], (err, result) => {
             console.log(result);
-        })
-        // db.query(`INSERT INTO role (title, salary, department) VALUES (?)`, newRole, (err, result) => {
-        //     if (err) {
-        //       console.log(err);
-        //     }
-        //     console.log(result);
-        //   });
-        })
-}
-
-const addDepartment = () => {
-    console.log('===============Add a New Department===============');
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the new Department?',
-            name: 'department',
-        },
-    ])
-    .then((response) => {
-        const newDept = response.department;
-        test(newDept);
+        }) 
     })
-}
-
-module.exports = {
-    addEmployee,
-    addRole,
-    // addDepartment,
 };
+
+updateRole();
